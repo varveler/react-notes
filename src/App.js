8cb58d7a7023a9897a02ea33a3dc5b0fe58b7f39
varveler <evarval@gmail.com>
2019-09-07 12:11:20 -0500

    added func to show notes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import NotesForm from './NotesForm'
+import NotesForm from './NotesForm';
+import NotesList from './NotesList';
 
-function App() {
-  return (
-    <Fragment>
-      <Typography align='center' variant='h2' gutterBottom>
-        Hello World
-      </Typography>
 
-      <Grid container justify='center' spacing={2}>
-        <Grid item xs={4}>
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      description:'',
+      notes: []
+    };
+  };
+  updateFields = field => event =>{
+    //console.log(event.target.value); //{/* e.target es el padre que llama el evento, en este caso es imput */}
+    this.setState({
+      [field]: event.target.value
+    })
+  // updateValue = event =>{
+  //   //console.log(event.target.value); //{/* e.target es el padre que llama el evento, en este caso es imput */}
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+
+
+    //console.log(this.state.value);
+  };
+  saveNote = (e) => {
+    if(this.state.title.trim() && this.state.description.trim()){
+      this.setState({
+        title:'',
+        description: '',
+        notes: [
+          ...this.state.notes,
+          {
+          id: Date.now(),
+          title: this.state.title,
+          description: this.state.description
+          }
+        ],
+        errors: {}
+      });
+    }
+  }
+  render(){
+    console.log(this.state)
+    return (
+      <Fragment>
+        <Typography align='center' variant='h2' gutterBottom>
+          Hello World
+        </Typography>
+        <Grid container justify='center' spacing={2}>
+          <Grid item xs={4}>
+            <NotesList notes={this.state.notes}/>
+          </Grid>
+          <Grid item xs={8}>
+            <NotesForm
+            title={this.state.title}
+            description={this.state.description}
+            //updateValue={this.updateValue}
+            updateFields = {this.updateFields}
+            saveNote = {this.saveNote}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={8}>
-          <NotesForm/>
-        </Grid>
-      </Grid>
-      <Fab color='primary' className='addIcon'>
-        <AddIcon/>
-      </Fab>
-    </Fragment>
-  );
+        <Fab color='primary' className='addIcon'>
+          <AddIcon/>
+        </Fab>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
--- src/NotesForm.js
+++ src/NotesForm.js
@@ -4,21 +4,41 @@ import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import Icon from '@material-ui/core/Icon';
 
-const NotesForm = () => {
+const NotesForm = ({title, description, updateFields, saveNote}) => {
   return(
     <Fragment>
+    <form onSubmit = {e => {
+      e.preventDefault();
+      {/*console.log(e.target);  e.target es el padre que llama el evento */}
+      saveNote()
+    }}>
       <Grid item xs={12}>
-        <TextField type='text' label='titulo' margin='normal' fullWidth/>
+          <TextField
+          required
+          name='title'
+          type='text'
+          label='titulo'
+          margin='normal'
+          // onChange = {props.updateValue}/>
+          onChange = {updateFields('title')}
+          value={title}
+          fullWidth/>
       </Grid>
-      <Grid item xs={12}>
-        <TextField
+        <Grid item xs={12}>
+          <TextField
+          name='description'
           multiline
           rows='4'
-         margin='normal'
-         fullWidth
-         placeholder='anita lava la tina'/>
-      </Grid>
-      <Fab color='secondary' className='editIcon'>
+           margin='normal'
+           fullWidth
+           placeholder='anita lava la tina'
+           onChange = {updateFields('description')}//  Material UI encapsula la funcion asi: (e) => updateField('description')(e)
+           value={description}
+           // onChange = {props.updateValue}
+           />
+        </Grid>
+        </form>
+      <Fab color='secondary' className='editIcon' onClick={saveNote}>
         <Icon>edit_icon</Icon>
       </Fab>
     </Fragment>
