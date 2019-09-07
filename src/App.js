120da9355a7410c4034a7eb953acb2332774c8b5
varveler <evarval@gmail.com>
2019-09-07 10:09:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import NotesForm from './NotesForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Typography align='center' variant='h2' gutterBottom>
+        Hello World
+      </Typography>
+
+      <Grid container justify='center' spacing={2}>
+        <Grid item xs={4}>
+        </Grid>
+        <Grid item xs={8}>
+          <NotesForm/>
+        </Grid>
+      </Grid>
+      <Fab color='primary' className='addIcon'>
+        <AddIcon/>
+      </Fab>
+    </Fragment>
   );
 }
 
